3a262294b0574b47008a1235280ae942d200062f
AbhijitKumarPandey <156564538+AbhijitKumarPandey@users.noreply.github.com>
2024-06-04 11:49:32 +0530

    Remove_One_Elements

---
+++ Array_List_Remove_Elements.java
@@ -0,0 +1,19 @@
+package Collection_Framework;
+import java.util.*;
+public class Array_List_Remove_Elements {
+	public static void main(String[] args) {
+		System.out.println("Well Come To Java collectio Framework ");
+		
+		ArrayList<String> name = new ArrayList<String>();
+		name.add("Suruchi");
+		name.add("Aastha");
+		name.add("Anjali");
+		name.add("Khusboo");
+		
+		System.out.println(name);
+		name.remove(1);
+		
+		System.out.println(name);
+	}
+
+}
