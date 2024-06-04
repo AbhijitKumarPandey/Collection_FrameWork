1786571070a401d6bc08cffa731cfbfb629c1709
AbhijitKumarPandey <156564538+AbhijitKumarPandey@users.noreply.github.com>
2024-06-04 11:56:32 +0530

    Check_Elements_there_is_no

---
+++ Array_List_Check_Element_there_is_no.java
@@ -0,0 +1,18 @@
+package Collection_Framework;
+
+import java.util.*;
+
+public class Array_List_Check_Element_there_is_no {
+	public static void main(String[] args) {
+		System.out.println("Well Come to Java code Thre is no");
+		ArrayList<String> name = new ArrayList<String>();
+		name.add("Abhijit");
+		name.add("Sumit");
+		name.add("Abhy");
+		System.out.println(name);
+		
+		System.out.println(name.contains("Sonu"));
+		
+	}
+
+}
