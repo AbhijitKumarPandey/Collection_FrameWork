38c608c9790ddc2bcb54070e4eee433513fcb326
AbhijitKumarPandey <156564538+AbhijitKumarPandey@users.noreply.github.com>
2024-06-04 11:53:29 +0530

    Empty_Method

---
+++ Check_For_Empty.java
@@ -0,0 +1,16 @@
+package Collection_Framework;
+import java.util.*;
+
+public class Check_For_Empty {
+	public static void main(String[] args) {
+		System.out.println("Well Come to java Collection framework Empty Method()");
+		ArrayList<String> name = new ArrayList<String>();
+		name.add("Abhijit");
+		name.add("Gudu");
+		name.add("Ajit");
+		System.out.println(name);
+		
+		System.out.println(name.isEmpty());
+	}
+
+}
