694d64168484c941f5de062afbbb4e487804f31f
AbhijitKumarPandey <156564538+AbhijitKumarPandey@users.noreply.github.com>
2024-06-04 11:47:06 +0530

    Q_1

---
+++ Array_List.java
@@ -0,0 +1,17 @@
+package Collection_Framework;
+import java.util.*;
+public class Array_List {
+	public static void main(String[] args) {
+		System.out.println("Well Come to Java Collection Framework Code");
+		ArrayList<String> name = new ArrayList<String>();
+		name.add("Abhijit");
+		name.add("Sumit");
+		name.add("Abhay");
+		name.add("Laptop");
+		 
+		System.out.println(name);
+		System.out.println(name.get(0));
+		System.out.println(name.get(1));
+	}
+
+}
