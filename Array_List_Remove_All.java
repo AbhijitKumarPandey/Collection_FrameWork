6e7163aa3dddefeedd9d386418bcd2dcfd75c490
AbhijitKumarPandey <156564538+AbhijitKumarPandey@users.noreply.github.com>
2024-06-04 11:48:11 +0530

    Remove_All_Elements

---
+++ Array_List_Remove_All.java
@@ -0,0 +1,18 @@
+package Collection_Framework;
+import java.util.*;
+public class Array_List_Remove_All {
+	 public static void main(String[] args) {
+		System.out.println("Well Come to Collection framework remove_All");
+		ArrayList<String> name = new ArrayList<String>();
+		name.add("Abhijit");
+		name.add("Abhay");
+		name.add("Anjali");
+		System.out.println(name);
+		
+		name.clear();
+		System.out.println(name);
+	 
+		
+	}
+
+}
