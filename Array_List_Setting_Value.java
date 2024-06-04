2ef9dc25aae85fead9097a0bf61c1773f3ad57e3
AbhijitKumarPandey <156564538+AbhijitKumarPandey@users.noreply.github.com>
2024-06-04 11:50:25 +0530

    Setting_Value

---
+++ Array_List_Setting_Value.java
@@ -0,0 +1,19 @@
+package Collection_Framework;
+import java.util.*;
+public class Array_List_Setting_Value {
+	public static void main(String[] args) {
+		System.out.println("Well Come To Java Collectio framework Setting Metho()");
+		
+		ArrayList<String> name = new ArrayList<String>();
+		name.add("Abhijt");
+		name.add("Cow");
+		name.add("Mobile");
+		name.add("Cricket");
+		System.out.println(name);
+		
+		//name.set(1, "Suruchi");
+		name.set(1, "Suruchi");
+		System.out.println(name);
+	}
+
+}
