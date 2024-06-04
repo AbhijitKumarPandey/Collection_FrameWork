e63c459afa7bfaf4df2691ce1ae556d6f5a501de
AbhijitKumarPandey <156564538+AbhijitKumarPandey@users.noreply.github.com>
2024-06-04 11:51:43 +0530

    Check_Size

---
+++ Array_List_Size.java
@@ -0,0 +1,18 @@
+package Collection_Framework;
+import java.util.*;
+public class Array_List_Size {
+	public static void main(String[] args) {
+		System.out.println("Well Come To Java Collectio Framework Size Method");
+		
+		ArrayList<String> name = new ArrayList<String>();
+		name.add("Abhijit");
+		name.add("Laptop");
+		name.add("Monitor");
+		name.add("book");
+		System.out.println(name);
+		
+		System.out.println("Sizeb = " + name.size());
+		 
+	}
+
+}
